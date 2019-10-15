e8feb9aeab50e08f509425a86f5d7b9fc1f932d2
jostergren79 <52670714+jostergren79@users.noreply.github.com>
2019-10-15 10:05:29 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  Article: require("./Article"),
-  Note: require("./Note")
-=======
   Note: require("./Note"),
   User: require("./User")
->>>>>>> b6457a4bf950f1cbc1d7169214ca55d450724999
 };
