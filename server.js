9c4afbf0170203447a5634e060722b4c5ca3bd19
Jason Ostergren <jason@stp-er-10-129-15-162.wireless.umn.edu>
2019-10-15 10:02:17 -0500

    updated with favicon.ico

--- server.js
+++ server.js
@@ -3,85 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
-<<<<<<< HEAD
-const db = require("./models");
-
-const PORT = 3000;
-const app = express();
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.wsj.com/").then(function(response) {
-    const $ = cheerio.load(response.data);
-
-    $("article h2").each(function(i, element) {
-      const result = {};
-
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    });
-
-    res.send("Scrape Complete");
-  });
-});
-
-app.get("/articles", function(req, res) {
-  db.Article.find({})
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.get("/articles/:id", function(req, res) {
-  db.Article.findOne({ _id: req.params.id })
-    .populate("note")
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
-=======
 
 const db = require("./models");
 
@@ -172,4 +93,3 @@ app.post("/articles/:id", function (req, res) {
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
->>>>>>> b6457a4bf950f1cbc1d7169214ca55d450724999
